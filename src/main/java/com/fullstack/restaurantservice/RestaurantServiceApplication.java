68f41fdaaa193db839b50d279a906893e8677151
Bryan <bryan.p.connelly@gmail.com>
2023-12-05 09:12:54 -0700

    fixed seatCustomer for demo

--- src/main/java/com/fullstack/restaurantservice/RestaurantServiceApplication.java
+++ src/main/java/com/fullstack/restaurantservice/RestaurantServiceApplication.java
@@ -44,12 +44,12 @@ public class RestaurantServiceApplication extends SpringBootServletInitializer {
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/seatCustomer")
-    public String seatCustomer(@RequestParam("firstName") String firstName,
+    public CustomerRecord seatCustomer(@RequestParam("firstName") String firstName,
                                                        @RequestParam("address") String address,
                                                        @RequestParam("cash") Float cash) throws EntityNotFoundException {
         log.debug("seatCustomer requested");
-//        return restaurantLogic.seatCustomer(firstName, address, cash);
-        return "customer was probably seated!";
+        return restaurantLogic.seatCustomer(firstName, address, cash);
+//        return "customer was probably seated!";
     }
 
     @ResponseStatus(HttpStatus.OK)
