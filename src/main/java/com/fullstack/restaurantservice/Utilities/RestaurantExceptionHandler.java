15db8a4db6a75c39bfbfe2c1663958f866d6bd8c
Bryan <bryan.p.connelly@gmail.com>
2024-03-13 18:27:59 -0600

    fixed string formatting in RestaurantExceptionHandler

--- src/main/java/com/fullstack/restaurantservice/Utilities/RestaurantExceptionHandler.java
+++ src/main/java/com/fullstack/restaurantservice/Utilities/RestaurantExceptionHandler.java
@@ -13,14 +13,14 @@ public class RestaurantExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> entityNotFoundHandler(EntityNotFoundException exception){
-        log.error(String.format("Exception: %s, message: %s"), exception.getCause(), exception.getMessage());
+        log.error(String.format("Exception: %s, message: %s", exception.getCause(), exception.getMessage()));
         if(exception.getMessage().isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("entity not found");
         else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getCause() + ", entity not found | " + exception.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exceptionHandler(Exception exception){
-        log.error(String.format("Exception: %s, message: %s"), exception.getCause(), exception.getMessage());
+        log.error(String.format("Exception: %s, message: %s", exception.getCause(), exception.getMessage()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getCause() + ", " + exception.getMessage());
     }
 }
