a8ebafbec163e5587f15da989c775ae2f0a02b4f
Bryan <bryan.p.connelly@gmail.com>
2024-03-25 23:55:58 -0600

    Removed @async stuff. No functionality benefits from it

--- src/main/java/com/fullstack/restaurantservice/DomainLogic/RestaurantLogic.java
+++ src/main/java/com/fullstack/restaurantservice/DomainLogic/RestaurantLogic.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
-//TODO: handle exceptions thrown in threads that don't have void return type
 @Slf4j
 @Service
 public class RestaurantLogic {
@@ -24,38 +21,32 @@ public class RestaurantLogic {
 
     private RestFetcher restFetcher;
 
-    public CompletableFuture<CustomerRecord> seatCustomer(String firstName, String address, Float cash) throws EntityNotFoundException, ExecutionException, InterruptedException {
-        List<TableRecord> openTables = getOpenTables().get();
+    public CustomerRecord seatCustomer(String firstName, String address, Float cash) throws EntityNotFoundException {
+        List<TableRecord> openTables = getOpenTables();
         if(openTables.isEmpty()) throw new EntityNotFoundException("no empty tables");
 
         return restFetcher.seatCustomer(firstName, address, cash, openTables.get(0).tableNumber());
     }
 
-    public CompletableFuture<List<TableRecord>> getOpenTables() throws EntityNotFoundException, ExecutionException, InterruptedException {
-        CompletableFuture<List<CustomerRecord>> allCustomersFuture = restFetcher.getAllCustomers();
-        CompletableFuture<List<TableRecord>> allTablesFuture = restFetcher.getAllTables();
-        CompletableFuture.allOf(allTablesFuture, allCustomersFuture).join();
-
-        List<TableRecord> allTables = allTablesFuture.get();
-        List<CustomerRecord> allCustomers = allCustomersFuture.get();
-
-        if(allTables.isEmpty()) throw new RuntimeException("no tables");
+    public List<TableRecord> getOpenTables() throws EntityNotFoundException {
+        List<TableRecord> allTables = restFetcher.getAllTables();
+        List<CustomerRecord> allCustomers = restFetcher.getAllCustomers();
 
         for(CustomerRecord customer:allCustomers){
             allTables.removeIf(n -> Objects.equals(n.tableNumber(), customer.tableNumber()));
         }
-        return CompletableFuture.completedFuture(allTables);
+        return allTables;
     }
 
-    public CompletableFuture<OrderRecord> submitOrder(String firstName, String dish, Integer tableNumber, Float bill) throws EntityNotFoundException, RuntimeException, ExecutionException, InterruptedException {
-        CustomerRecord customer = restFetcher.getCustomerByName(firstName).get();
+    public OrderRecord submitOrder(String firstName, String dish, Integer tableNumber, Float bill) throws EntityNotFoundException, RuntimeException {
+        CustomerRecord customer = restFetcher.getCustomerByName(firstName);
         if(customer.cash() < bill) throw new RuntimeException("customer has insufficient funds");
         return restFetcher.submitOrder(firstName, dish, tableNumber, bill);
     }
 
-    public CompletableFuture<CustomerRecord> bootCustomer(String firstName) throws EntityNotFoundException, ExecutionException, InterruptedException {
-        CompletableFuture<CustomerRecord> bootedCustomer = restFetcher.bootCustomer(firstName);
-        return CompletableFuture.completedFuture(bootedCustomer.get());
+    public CustomerRecord bootCustomer(String firstName) throws EntityNotFoundException {
+        CustomerRecord bootedCustomer = restFetcher.bootCustomer(firstName);
+        return bootedCustomer;
 
     }
 }
