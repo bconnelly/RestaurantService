2602d5c51e8387ecc2748e98fd0812bec944210e
Bryan <bryan.p.connelly@gmail.com>
2025-03-05 22:29:38 -0700

    !refactor: Simplified service endpoints, Mockito config
    "base" properties were unnecessary, service endpoint nouns are very unlikely to change.
     Added Mockito jar as arg in surefire as instructed in Mockito documentation

--- src/main/java/com/fullstack/restaurantservice/DataEntities/OrderRecord.java
+++ src/main/java/com/fullstack/restaurantservice/DataEntities/OrderRecord.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record OrderRecord(@NotNull String firstName, @NotNull Integer tableNumber, @NotNull String dish, Float bill) {
     public boolean equals(OrderRecord comparedRecord) {
-        return (this.firstName.equals(comparedRecord.firstName()) &&
+        return (this.firstName.equals(comparedRecord.firstName) &&
         this.tableNumber.equals(comparedRecord.tableNumber) &&
         this.dish.equals(comparedRecord.dish) &&
         this.bill.equals(comparedRecord.bill));
